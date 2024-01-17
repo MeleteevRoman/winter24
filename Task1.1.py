18044558235c9eb5686f60603a282af87ce3cf06
dns <donor8888@gmail.com>
2024-01-17 14:23:18 +0300

    version 1.0

--- Task1.1.py
+++ Task1.1.py
@@ -19,15 +19,15 @@ b=x-y
 c=x*y
 d=x/y
 e=x//y
-if a>b and a>=c and a>d and a>e:
+if a>=b and a>=c and a>=d and a>=e:
     print(f"Наибольшее число x+y: {a}")
-if b>a and b>c and b>d and b>e:
+if b>=a and b>=c and b>=d and b>=e:
     print(f"Наибольшее число x-y: {b}")
-if d>a and d>b and d>c and d>=e:
+if d>a and d>=b and d>=c and d>=e:
     print(f"Наибольшее число x/y: {d}")
-if e>a and e>b and e>c and e>=d:
+if e>=a and e>=b and e>=c and e>=d:
     print(f"Наибольшее число x//y: {e}")
-if c>=a and c>b and c>d and c>e:
+if c>=a and c>=b and c>=d and c>=e:
     print(f"Наибольшее число x*y: {c}")
 print()
 
