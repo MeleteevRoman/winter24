2eb55ce9b98e3c1b8a77f38cd8e374fcf9e43324
dns <donor8888@gmail.com>
2024-02-10 21:15:37 +0300

    version 1.0

---
+++ Task12.py
@@ -0,0 +1,25 @@
+# Задание 12-1
+x=list(input('Введите список чисел: '))
+print(x)
+max_num=max(x)
+min_num=min(x)
+max_ind=[index for index, i in  enumerate(x) if i==max_num]
+min_ind=[index for index, i in  enumerate(x) if i==min_num]
+print(f' Минимальное число списка:{min_num}\n',
+      f'Максимальное число списка:{max_num}')
+print(f'Список индексов элементов равных\n '
+      f'минимальному значению списка Х:{min_ind}')
+print(f'Список индексов элементов равных\n '
+      f'максимальному значению списка Х:{max_ind}')
+
+# Задание 12-2
+a=[x for x in range(1,11) for i in range(x) ]
+print(a)
+
+
+s=[1,2,3,4,5,6,7,8,9,10]
+res=[]
+for w in s:
+    for i in range(w):
+        res.append(w)
+print(res)
