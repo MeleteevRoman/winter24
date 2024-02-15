9f55a80cabf7c8cd2aa9ec872bad96087e297444
dns <donor8888@gmail.com>
2024-02-15 19:22:40 +0300

    version 1.0

---
+++ Task14.py
@@ -0,0 +1,33 @@
+# Задание 14-1.
+def sumd(n):
+    if n>=0:
+        print(len(str(n)))
+    else:
+        sumd(-n)
+    return n
+
+n = int(input('Введите натуральное число: '))
+sumd(n)
+
+# Задание 14-2.
+def sum_of_digits(n):
+    if n < 10:
+        return n
+    else:
+        return n % 10 + sum_of_digits(n // 10)
+n = int(input('Введите натуральное число: '))
+print(sum_of_digits(n))
+
+# Задание 14-3.
+def tri_2(n):
+    print('*'* n)
+    n-=1
+    if n>=1:
+        tri_2(n)
+        n+=1
+    if n==0:
+        return
+    print('*'* n)
+
+    return
+tri_2(int(input("Введите размер треугольников: ")))
