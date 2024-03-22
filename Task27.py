5bc7018d1cad12b4b7da3402bc34e288f0caed79
dns <donor8888@gmail.com>
2024-03-22 16:37:00 +0300

    version 1.0

---
+++ Task27.py
@@ -0,0 +1,14 @@
+#Задание 27-1
+def dartboard(n):
+    row, top = 0, []
+    for i in range(n, 0, -2):
+        row += int('{:0^{}}'.format('1'*i, n))
+        top.append(row)
+
+        bottom = top[::-1][1:] if n%2 else top[::-1]
+
+
+    return top + bottom
+
+
+print(*dartboard(int(input("Введите число: "))),sep="\n")
